f1347baf1e1742082fe5f4a735e5af941bac2410
yasminhana <92916680+yasminhana@users.noreply.github.com>
2022-06-10 15:09:31 +0800

    Update casestudycalc.dart

--- casestudycalc.dart
+++ casestudycalc.dart
@@ -22,27 +22,32 @@ class Storage {
   }
 
   //function
-  void storecalc(var house,var num) {
-    var i=0;
-    if (housenum[house-1][4] != 0) {
+  void storecalc(var house, var num) {
+    var i = 0;
+    if (housenum[house - 1][4] != 0) {
       print('Storage full!!!');
-    }else{
-      for (var j = i; j < i+1; j++) {
-        if (housenum[house-1][j] == 0) {
-          housenum[house-1][j] = num;
-        }
-        else{
+    } else {
+      for (var j = i; j < i + 1; j++) {
+        if (housenum[house - 1][j] == 0) {
+          housenum[house - 1][j] = num;
+        } else {
           i++;
         }
       }
     }
-    
-    print(housenum[house-1]);
+
+    print(housenum[house - 1]);
+
+    var entered_at = new DateTime.now();
+    var overdued = entered_at.add(new Duration(hours: 48));
+
+    print("Date and time parcel entry is at $entered_at");
+    print("Parcel will be returned to sender after $overdued");
   }
 
   void display() {
     for (var i = 1; i <= 30; i++) {
-      var newhousenum = housenum[i-1];
+      var newhousenum = housenum[i - 1];
       print('House [$i] = $newhousenum');
     }
   }
